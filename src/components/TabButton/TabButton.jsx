a1f2a9210c5461be9e09a47e70249673d52d7a54
Alberto <garcia.lara.alberto@gmail.com>
2024-07-10 14:16:55 +0200

    Tab button uses now forwarded props

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -4,6 +4,6 @@ function TabButtonOriginal (props) {
   return <li><button>{props.children}</button></li>
 }
 
-export default function TabButton ({children, onSelect, isSelected}) {
-  return <li><button className={ isSelected ? 'active'  : undefined} onClick={onSelect}>{children}</button></li>
+export default function TabButton ({children, isSelected, ...props}) {
+  return <li><button className={ isSelected ? 'active'  : undefined} {...props}>{children}</button></li>
 }
